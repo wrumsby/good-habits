c0def1962ba6cd1fa39f22d133e7bcb019d0ffa4
Walter Rumsby <walter@rumsby.net>
2014-02-10 13:20:52 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Good Habits for JavaScript Programmers
+
+Wellington.js Presentation
+
+[View Slides](http://zenp.azurewebsites.net/index.html?slides=https://raw.github.com/wrumsby/good-habits/master/) and [notes](http://zenp.azurewebsites.net/notes.html?slides=https://raw.github.com/wrumsby/good-habits/master/) (this will probably only work in Chrome ATM) 
---
+++ slides.js
@@ -0,0 +1,164 @@
+var SLIDES = [
+	{
+		"cls": "first",
+		"title": "Good Habits for JavaScript Programmers",
+		"notes": "My opinion on how you should write JavaScript (scale)"
+	},
+
+	{
+		"cls": "first",
+		"title": "Hi",
+		"notes": "Me; Discipline Lead FED for Xero in NZ (Xero growth, etc.)"
+	},
+
+	{
+		"quote": {
+			"text": "[I used to think] JavaScript isn't a serious programming language",
+			"from": "..."
+		},
+		"image": "http://www.flickr.com/photos/centermez/7104203537/"
+	},
+
+	{
+		"quote": {
+			"text": "...so I don't need to apply the same software development principles that I studied and diligently apply when I'm developing in a *proper* language such as C or Java",
+			"from": "..."
+		},
+		"image": "http://www.flickr.com/photos/centermez/7104203537/"
+	},
+
+	{
+		"quote": {
+			"text": "Now that I put it like that I shouldn't be surprised that every large JavaScript project I work on becomes a steaming pile.",
+			"from": "Karl von Randow"
+		},
+		"image": "http://www.flickr.com/photos/centermez/7104203537/",
+		"notes": "what works at small scale doesn't necessarily work at large scale; this is just good practice"
+	},
+
+	{
+		"quote": {
+			"text": "I'm not a great programmer; I'm just a good programmer with great habits.",
+			"from": "Kent Beck"
+		},
+		"image": "http://www.flickr.com/photos/26475170@N02/5971736586/"
+	},
+
+	{
+		"title": "Developer Experience",
+		"image": "http://www.flickr.com/photos/gagilas/5825241157/",
+		"notes": "Documentation, lest surprise, no magic, devexp patterns (maybe this should be the start?); implicit knowledge"
+	},
+
+	{
+		"title": "Review Each Other's Code",
+		"note": "Github is sweet, great for folks in different locations; Joel test; eliminate noise",
+		"image": "http://www.flickr.com/photos/sebastian_bergmann/3991539605/"
+	},
+
+	{
+		"title": "[Only] Source in Source Control",
+		"image": "http://www.flickr.com/photos/crystalflickr/42095174/",
+		"notes": ".gitignore, reduce merge conflicts, eliminate unnecessary conflicts"
+	},
+
+	// TODO: gist
+
+	{
+		"title": "Significant Whitespace",
+		"image": "http://www.flickr.com/photos/withassociates/2978570304/",
+		"notes": ".editorconfig; .gitattributes; both of these are about making things easier for reviewers"
+	},
+	
+	{
+		"gist": "https://gist.github.com/wrumsby/8854824"
+	},
+
+	{
+		"quote": {
+			"text": "The Web is the most hostile software engineering environment imaginable",
+			"from": "Douglas Crockford"
+		},
+		"image": "http://www.flickr.com/photos/olibac/2472896028/",
+		"notes": "FIX YOUR CODE!"
+	},
+
+	{
+		"title": "Lint your JavaScript",
+		"image": "http://www.flickr.com/photos/bunnyfrogs/5110760943/",
+		"notes": "JSHint, .jshintrc, .jshintignore, grunt-contrib-jshint, pull request build, ES Lint"
+	},
+
+	{
+		"gist": "https://gist.github.com/wrumsby/8854810"
+	},
+
+	{
+		"gist": "https://gist.github.com/wrumsby/8854815"
+	},
+
+	{
+		"quote": {
+			"text":	"The secret to building large apps is never build large apps. Break your applications into small pieces. Then, assemble those testable,  bite-sized pieces into your big application",
+			"from":	"Justin Meyer, JavaScript MVC"
+		},
+		"image": "http://www.flickr.com/photos/62472560@N00/4679740934/"
+	},
+
+	{
+		"title": "Use Modules",
+		"image": "http://www.flickr.com/photos/ogimogi/2253657555/",
+		"notes": "module systems - RequireJS, Browserify, "
+	},
+
+	{
+		"gist": "https://gist.github.com/wrumsby/5907620"
+	},
+
+	{
+		"quote": {
+			"text": "The best thing that can be said of [any piece of] software is that it is too small",
+			"from": "James Halliday aka @substack"
+		},
+		"image": "http://www.flickr.com/photos/theodorescott/8607310734/",
+		"notes": "unix philosophy - @substack, most modules on npm - when you do small things easy to add docs, README, etc"
+	},	
+
+	{
+		"title": "Write Unit Tests",
+		"notes": "PhantomJS, pull request build, other kinds of testing - Huxley (visual regression) vs Selenium (structure)",
+		"image": "http://www.flickr.com/photos/horiavarlan/4273224901/"
+	},
+
+	{
+		"gist": "https://gist.github.com/wrumsby/5335759"
+	},
+
+	{
+		"title": "README",
+		"image": "http://www.flickr.com/photos/76196395@N00/76196336/",
+		"notes": "Documentation other devexp ideas - writing documentation and tests helps shift your perspective"
+	},
+
+	{
+		"title": "Automate",
+		"image": "http://www.flickr.com/photos/gombao/77313/",
+		"notes": "Don't do work you don't need to; don't make others do work they don't need to"
+	},
+
+	{
+		"title": "Be Excellent to Each Other",
+		"image": "http://www.flickr.com/photos/st3f4n/2144232279/",
+		"notes": "EMPATHY! Don't be a douchebag"
+	},
+
+	{
+		"title": "Talk to each other",
+		"notes": "hangouts, video, etc",
+		"image": "http://www.flickr.com/photos/53326337@N00/7048073333/"
+	},
+
+	{
+		credits: true
+	}
+];
